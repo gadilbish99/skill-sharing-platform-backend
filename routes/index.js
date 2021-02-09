b130c5bcfabb773fb172ba22985fc37cea0b6fc5
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-10 04:27:17 +0800

    Add MockAPI

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const mockPost = {
+  summary: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  title: "My Post",
+  image: "https://bit.ly/39VxniL"
+};
+
+const mockPosts = Array(10).fill(mockPost); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({msg: 'Got it.'});
+  console.log(req.headers)
+  res.send(mockPosts);
 });
 
 module.exports = router;
