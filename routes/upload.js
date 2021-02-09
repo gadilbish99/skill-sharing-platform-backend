3939969486666670fd43ccc6c4d5e2057e1a3d73
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-10 04:23:33 +0800

    Add upload.js

---
+++ routes/upload.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function(req, file, cb){
+     cb(null,"image" + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 10000000}
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send('uploads/' + req.file.filename);
+});
+
+module.exports = router;
