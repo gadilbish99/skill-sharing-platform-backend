48269adead0b8bc7afff413089e1b484d320b673
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-11 00:13:50 +0800

    Add Routers to indexRouter

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const userRouter = require('./user');
+const usersRouter = require('./users');
+const uploadRouter = require('./upload');
+const postRouter = require('./post');
+const postsRouter = require('./posts');
 
-router.get('/', async function(req, res, next) {
-  const db = req.app.locals.db;
-  const posts = await db.getAllPosts();
-  res.send(posts);
-});
+router.use('/post', postRouter);
+router.use('/posts', postsRouter);
+router.use('/upload', uploadRouter);
+router.use('/user', userRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
 
