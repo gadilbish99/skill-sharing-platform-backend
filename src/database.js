c9941d6b8ef8b57bbccf050d00f14fc81d0126f0
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-10 15:21:55 +0800

    Add User Methods

--- src/database.js
+++ src/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const fs = require('fs')
-const path = require('path');
 const PostModel = require('./db/post.js');
+const UserModel = require('./db/user.js');
 
 const server = 'localhost:27017'; // REPLACE WITH YOUR DB SERVER
 const database = 'skill-sharing-platform';      // REPLACE WITH YOUR DB NAME
@@ -26,7 +25,7 @@ class Database {
       })
   }
 
-  // User methods
+  // Blog Post methods
   async addPost(newPost) {    
     const post = await PostModel.create(newPost);
     return post;
@@ -40,6 +39,32 @@ class Database {
   async findPostByID(id) {
     const post = await PostModel.findById(id);
     return post;
+  }
+
+  // User methods
+  async addUser(newUser) {    
+    const user = await UserModel.create(newUser);
+    return user;
+  }
+  
+  async getAllUsers() {
+    const users = await UserModel.find();
+    return users;
+  }
+
+  async findUser(email) {
+    const user = await UserModel.findOne({ email: email });
+    return user;
+  }
+
+  async findUserByID(id) {
+    const user = await UserModel.findById(id);
+    return user;
+  }
+
+  async setToken(id, refresh_token) {
+    const user = await UserModel.findByIdAndUpdate(id, { refresh_token: refresh_token });
+    return user;
   }
 }
 
