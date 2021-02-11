09dcae632789fcd1cab60f19c2c767ac8c502d9b
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-11 13:42:22 +0800

    Improve Post router

--- routes/post.js
+++ routes/post.js
@@ -1,16 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { isAuth } = require('../src/isAuth');
 
-router.post('/', async function(req, res, next) {
-  const db = req.app.locals.db;
-  const post = await db.addPost(req.body);
-  res.send('Submitted' + post);
+router.post('/', async function(req, res) {  
+  try {
+    const userId = isAuth(req);
+    if (userId !== null) {
+      const db = req.app.locals.db;
+      const post = await db.addPost(req.body);
+      res.send({
+        msg: 'Submitted Post ' + post.id
+      });
+    }
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+    });
+  }
 });
 
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', async function(req, res) {
   const db = req.app.locals.db;
   const post = await db.findPostByID(req.params.id);
   res.send(post);
 });
+
+router.delete('/:id', async function(req, res) {
+  const db = req.app.locals.db;
+  const post = await db.deletePostByID(req.params.id);
+  res.send('Deleted Post ' + req.params.id);
+});
+
 
 module.exports = router;
