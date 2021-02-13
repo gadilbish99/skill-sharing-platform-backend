a48c748506914126a1de60980ccd9e872e02ad38
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-13 22:43:33 +0800

    Add auth to usersRouter

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
-router.get('/', async function(req, res, next) {
+router.get('/', auth, async function(req, res, next) {
   const db = req.app.locals.db;
   const users = await db.getAllUsers();
   res.send(users);
