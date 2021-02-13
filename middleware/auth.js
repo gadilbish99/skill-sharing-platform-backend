12d67c5282e35d51cb3cc523173c50ca3dc59e63
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-13 13:04:38 +0800

    Add auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,25 @@
 const { verify } = require('jsonwebtoken');
 
-const isAuth = (req) => {
+const auth = (req, res, next) => {
   try {
     const authorization = req.headers['authorization'];
     if (!authorization) throw new Error('You need to login.');
     // Based on 'Bearer ksfljrewori384328289398432'
     const token = authorization.split(' ')[1];
     const { userId } = verify(token, process.env.ACCESS_TOKEN_SECRET);
-    return userId;
+    if (userId && userId !== null) {
+      req.userId = userId
+      next();
+    }
+    else 
+      throw new Error('Undefined user');
   } catch (err) {
-    throw err;
+    res.status(401).send({
+      error: `${err.message}`,
+    });
   }
 };
 
 module.exports = {
-  isAuth,
+  auth,
 };
